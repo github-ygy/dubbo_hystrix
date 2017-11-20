c7bf4aa8c2b28028f862aab2fdaf7792df2bd3a0
wb-ygy260316 <wb-ygy260316@alibaba-inc.com>
2017-11-20 19:25:08 +0800

    服务消费 参数注释

--- io_web/src/main/java/ygy/test/dubbo/hystrix/controller/RoleController.java
+++ io_web/src/main/java/ygy/test/dubbo/hystrix/controller/RoleController.java
@@ -1,6 +1,6 @@
 package ygy.test.dubbo.hystrix.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ygy.test.dubbo.hystrix.service.role.RoleService;
@@ -12,8 +12,9 @@ import ygy.test.dubbo.hystrix.service.role.RoleService;
 public class RoleController {
 
 
-    @Reference(version = "1.0.0")
-    private RoleService roleService ;
+    //@Reference(version = "1.0.0")   //开启dubbo注解是使用
+    @Autowired
+    private RoleService roleService ;   //使用xml 配置时，可用autowired注入
 
     @RequestMapping("testConsumer")
     public Object testConsumer() {
