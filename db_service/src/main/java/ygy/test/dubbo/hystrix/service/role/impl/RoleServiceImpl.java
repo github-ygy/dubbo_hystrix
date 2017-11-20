f57f27bac91b985d47ca2d3e2b462270f0661079
ygy <ygyworkforit@163.com>
2017-11-21 00:17:48 +0800

    服务集群容灾测试

--- db_service/src/main/java/ygy/test/dubbo/hystrix/service/role/impl/RoleServiceImpl.java
+++ db_service/src/main/java/ygy/test/dubbo/hystrix/service/role/impl/RoleServiceImpl.java
@@ -1,6 +1,8 @@
 package ygy.test.dubbo.hystrix.service.role.impl;
 
 import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ygy.test.dubbo.hystrix.dto.StudentDto;
 import ygy.test.dubbo.hystrix.service.role.RoleService;
@@ -14,14 +16,25 @@ import java.util.List;
 @com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")
 public class RoleServiceImpl implements RoleService {
 
+    private static final Logger log=LoggerFactory.getLogger(RoleServiceImpl.class);
+
+    String modul="db_service";
+
     @Override
-    public List<StudentDto> getAllStudent() {
+    public List<StudentDto> getAllStudent()  {
+        log.info(" getAllStudent invoke " + modul);
         List<StudentDto> sLists=Lists.newArrayList();
         StudentDto studentDto=new StudentDto();
-        studentDto.setName("ygy");
+        studentDto.setName("test");
         studentDto.setAge(25);
         studentDto.setSex(1);
         sLists.add(studentDto);
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+
+        }
+
         return sLists;
     }
 }
