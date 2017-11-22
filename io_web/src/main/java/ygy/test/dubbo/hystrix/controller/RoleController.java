ca3b95ace11e26fe5d4804bedf1c90879dc7c034
wb-ygy260316 <wb-ygy260316@alibaba-inc.com>
2017-11-22 18:43:26 +0800

    异步调用

--- io_web/src/main/java/ygy/test/dubbo/hystrix/controller/RoleController.java
+++ io_web/src/main/java/ygy/test/dubbo/hystrix/controller/RoleController.java
@@ -1,15 +1,21 @@
 package ygy.test.dubbo.hystrix.controller;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ygy.test.dubbo.hystrix.dto.StudentDto;
 import ygy.test.dubbo.hystrix.service.role.RoleService;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by guoyao on 2017/11/20.
@@ -26,6 +32,22 @@ public class RoleController {
     @RequestMapping("testConsumer")
     public Object testConsumer() {
         return roleService.getAllStudent();
+    }
+
+    @RequestMapping("testAsyn")
+    public Object testAsyn(@RequestParam Integer id ) {
+        StudentDto stu=roleService.getByIdAsyc(id);
+        log.warn(" stu = " + stu);
+        Future<StudentDto> future=RpcContext.getContext().getFuture();
+        StudentDto studentDto=null;
+        try {
+            studentDto=future.get();
+            return studentDto ;
+        } catch (Exception e) {
+            log.error("query error ",e);
+        }
+
+        return "error";
     }
 
     @RequestMapping("testCache")
