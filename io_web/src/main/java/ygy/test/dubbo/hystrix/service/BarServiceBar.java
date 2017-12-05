c8f4ad080ba0c995bb424dcac02593715b5d4a57
ygy <ygyworkforit@163.com>
2017-12-05 22:59:53 +0800

    递归级联

--- io_web/src/main/java/ygy/test/dubbo/hystrix/service/BarServiceBar.java
+++ io_web/src/main/java/ygy/test/dubbo/hystrix/service/BarServiceBar.java
@@ -1,11 +1,15 @@
 package ygy.test.dubbo.hystrix.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ygy.test.dubbo.hystrix.dto.StudentDto;
 import ygy.test.dubbo.hystrix.service.role.BarService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -21,7 +25,7 @@ public class BarServiceBar implements BarService {
     @Override
     public List<StudentDto> selectByName(String name) {
         //计算降级次数
-        log.info(" - -- -- - - - - - rpc exception  mock :  " + name );
+        log.info(" - -- -- - - - - - rpc exception  mock :  " + name);
         log.warn(" ================= bar count " + ATOMIC_INTEGER.incrementAndGet());
 
         StudentDto studentDto=new StudentDto();
@@ -29,5 +33,50 @@ public class BarServiceBar implements BarService {
         studentDto.setSex(1);
         studentDto.setAge(1);
         return Lists.newArrayList(studentDto);
+    }
+
+
+    public static void main(String[] args) {
+
+        List<City> list=new ArrayList<>();
+        list.add(new City("1", "test1", null));
+        list.add(new City("2", "test2", "1"));
+        list.add(new City("3", "test3", "1"));
+        list.add(new City("4", "test4", "2"));
+        list.add(new City("5", "test5", "3"));
+        list.add(new City("6", "test6", "5"));
+        list.add(new City("7", "test7", "4"));
+        List resultList=new ArrayList<>();
+        build(list, null, resultList);
+        System.out.println(JSONObject.toJSON(resultList));
+    }
+
+
+    public static void build(List<City> llist, String id, List resultList) {
+        for (City city : llist) {
+            if (city.getPid()== id) {
+                HashMap<String, Object> dataMap=new HashMap<>();
+                dataMap.put("name", city.getName());
+                dataMap.put("id", city.getId());
+                List dataList=new ArrayList();
+                dataMap.put("children", dataList);
+                resultList.add(dataMap);
+                build(llist, city.getId(), dataList);
+            }
+        }
+    }
+}
+
+
+@Data
+class City {
+    private String id;
+    private String name;
+    private String pid;
+
+    City(String id, String name, String pid) {
+        this.id=id;
+        this.name=name;
+        this.pid=pid;
     }
 }
